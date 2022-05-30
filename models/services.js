0d7d798b8518e38cc1a0803ca9d9bf57eeb89c12
iya873 <siyana873@gmail.com>
2022-05-30 16:11:30 -0400

    connected my db can see json in browser

---
+++ models/services.js
@@ -0,0 +1,8 @@
+const services = [
+    { type: 'Face', serviceName: ['']},
+    { type: 'Body', serviceName: ['']},
+    { type: 'Waxing', serviceName: ['']},
+    { type: 'Products', img: ['https://cdn.shopify.com/s/files/1/0250/9140/7954/products/il_fullxfull.2972878105_lu7i_1024x1024@2x.jpg?v=1616212618', 'https://cdn.shopify.com/s/files/1/0250/9140/7954/products/il_fullxfull.2704346620_ovnb_1024x1024@2x.jpg?v=1616212618', 'https://cdn.shopify.com/s/files/1/0250/9140/7954/products/315E18DD-969D-483B-986C-15B8BF0699DD_1024x1024@2x.jpg?v=1627390861', 'https://cdn.shopify.com/s/files/1/0250/9140/7954/products/il_fullxfull.2925108854_ma5w_1024x1024@2x.jpg?v=1616212637']}
+]
+
+module.exports = services
--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const services = require('./models/services')
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to Services by Sweet Ambiance!')
+})
+
+app.get('/services', (req, res) => {
+    res.send(services)
 })
 
 app.listen(port, () => {
